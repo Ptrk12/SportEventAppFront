40d604d897410ac46b57ea6cbe5864181ba476dd
Patryk Bajak <Patryk.Bajak@microsoft.wsei.edu.pl>
2024-06-23 16:40:58 +0200

    working on home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import {
   SelectChangeEvent,
 } from "@mui/material";
 import { useState } from "react";
+import SearchIcon from '@mui/icons-material/Search';
 
 interface FormComponentProps {
   discipline: string;
@@ -21,7 +22,9 @@ const Home = () => {
     searchString: "",
   });
 
-  const onInputSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const onInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement> | SelectChangeEvent<string>
+  ) => {
     const { name, value } = event.target;
 
     setSearchFormValues((prevValues) => ({
@@ -29,36 +32,74 @@ const Home = () => {
       [name]: value,
     }));
   };
-
-  const handleDisciplineChange = (event: SelectChangeEvent<string>) => {
-    setSearchFormValues((prevValues) => ({
-      ...prevValues,
-      discipline: event.target.value as string,
-    }));
-  };
-
+console.log(searchFormValues.searchString)
   return (
     <div className="flex flex-col">
-      <div className="flex align-center justify-center">
-        <span>What would You like to do?</span>
-      </div>
-      <div className="flex align-center justify-center">
-        <div className="flex">
-          <FormControl className="w-[160px]">
-            <InputLabel id="discipline-select-label">Discipline</InputLabel>
-            <Select
-              labelId="discipline-select-label"
-              id="discipline-select"
-              value={searchFormValues.discipline}
-              name="discipline"
-              label="Discipline"
-              onChange={handleDisciplineChange}
-            >
-              <MenuItem value="football">Football</MenuItem>
-              <MenuItem value="volleyball">Volleyball</MenuItem>
-              <MenuItem value="basketball">Basketball</MenuItem>
-            </Select>
-          </FormControl>
+      <div className="relative bg-[url('/public/assets/home-bg.jpg')] h-[600px] w-full bg-cover bg-center bg-top">
+        <div className="relative flex align-center justify-center">
+          <div className="absolute top-[310px] left-[200px] flex align-center justify-center text-dark-text-color font-bold text-5xl">
+            <span>What would You like to do?</span>
+          </div>
+          <div className="absolute top-[370px] flex gap-16 align-center justify-center items-center p-3 bg-white rounded-[20px] w-[80%] shadow-xl">
+            <FormControl className="w-[350px]">
+              <InputLabel id="discipline-select-label">Discipline</InputLabel>
+              <Select
+                sx={{
+                  '& .MuiOutlinedInput-notchedOutline': {
+                    border: 'none',
+                  },
+                  '& .MuiSelect-select': {
+                    borderBottom: '1px solid gray',
+                  },
+                }}
+                labelId="discipline-select-label"
+                id="discipline-select"
+                value={searchFormValues.discipline}
+                name="discipline"
+                label="Discipline"
+                onChange={onInputChange}
+              >
+                <MenuItem value="football">Football</MenuItem>
+                <MenuItem value="volleyball">Volleyball</MenuItem>
+                <MenuItem value="basketball">Basketball</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl className="w-[350px]">
+              <InputLabel id="city-select-label">Location</InputLabel>
+              <Select
+                sx={{
+                  '& .MuiOutlinedInput-notchedOutline': {
+                    border: 'none',
+                  },
+                  '& .MuiSelect-select': {
+                    borderBottom: '1px solid gray',
+                  },
+                }}
+                labelId="city-select-label"
+                id="city-select"
+                value={searchFormValues.city}
+                name="city"
+                label="City"
+                onChange={onInputChange}
+              >
+                <MenuItem value="krakow">Kraków</MenuItem>
+                <MenuItem value="warszawa">Warszawa</MenuItem>
+                <MenuItem value="Gdansk">Gdańsk</MenuItem>
+              </Select>
+            </FormControl>
+            <div className="mt-[6px]">
+              <TextField
+                className="w-[380px]"
+                id="search-string"
+                label="Search: Krakow, MultiSport, z trenerem"
+                variant="standard"
+                name="searchString"
+                value={searchFormValues.searchString}
+                onChange={onInputChange}
+              />
+            </div>
+            <SearchIcon fontSize="large" style={{ color: '#5e2569' }} />
+          </div>
         </div>
       </div>
     </div>
