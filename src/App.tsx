8dae3b281e0925ed67ae47703bf126bfd39657cb
Michal Pietrasz <michalpietrasz2115@gmail.com>
2024-10-15 01:18:58 +0200

    loginregister

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import Home from './pages/Home';
 import Ranking from './pages/Ranking';
 import Events from './pages/Events';
 import About from './pages/About';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
   return (
-    <div>
+    <UserProvider>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/> 
-        <Route path='/events'  element={<Events/>}/> 
-        <Route path='/ranking'  element={<Ranking/>}/> 
-        <Route path='/about'  element={<About/>}/> 
-        <Route path='/chat'  element={<Home/>}/> 
+        <Route path='/' element={<Home />} />
+        <Route path='/events' element={<Events />} />
+        <Route path='/ranking' element={<Ranking />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
-    </div>
+    </UserProvider>
   );
 }
 
